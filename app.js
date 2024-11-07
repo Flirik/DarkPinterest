8f3f43799b1e7243d2283a62894330d8ad86c52e
Flirik <92211231+Flirik@users.noreply.github.com>
2024-11-08 00:23:55 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+if(document.querySelector("#__PWS_ROOT__ > div:nth-child(1) > div > div.appContent > div > div > div:nth-child(2)")){
+	document.querySelector("#__PWS_ROOT__ > div:nth-child(1) > div > div.appContent > div > div > div:nth-child(2)").style.background = 'transparent';
+}
+
